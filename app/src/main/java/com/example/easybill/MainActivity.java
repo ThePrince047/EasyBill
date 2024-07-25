ddd6908bc619a241f5c233157524b92b9f7b1c78
JayRangani <86279737+JayRangani@users.noreply.github.com>
2024-07-25 16:30:58 +0530

    New Design 1.1

--- app/src/main/java/com/example/easybill/MainActivity.java
+++ app/src/main/java/com/example/easybill/MainActivity.java
@@ -3,19 +3,30 @@ package com.example.easybill;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    String partyname[]={"A","b","C"},time[]={"A","b","C"},amount[]={"A","b","C"};
+    ListView listView;
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.invoice_list_item,R.id.lblPartyname,partyname);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +37,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        listView = findViewById(R.id.homeinvoicelist);
+        listView.setAdapter(adapter);
 
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Prince");
+        textView = findViewById(R.id.lblUser);
+       myRef.addValueEventListener(new ValueEventListener() {
+           @Override
+           public void onDataChange(@NonNull DataSnapshot snapshot) {
+               String value = snapshot.getValue(String.class);
+               textView.setText(value);
+           }
+
+           @Override
+           public void onCancelled(@NonNull DatabaseError error) {
+
+           }
+       });
     }
 }
